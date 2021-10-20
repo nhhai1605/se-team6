2fa2d29003a1cd09579f1c2b683f5529f4c05c47
s3822918 <s3822918@rmit.edu.vn>
2021-10-20 20:49:41 +1100

    fix test

--- BackEnd/reviewmicroservices/src/test/java/com/rmit/sept/reviewmicroservices/ReviewmicroservicesApplicationTests.java
+++ BackEnd/reviewmicroservices/src/test/java/com/rmit/sept/reviewmicroservices/ReviewmicroservicesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.reviewmicroservices;
+package com.rmit.sept.reviewmicroservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
